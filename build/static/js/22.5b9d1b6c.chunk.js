4d316a855a75c4307eaec69fac2b8a9325fc8537
hiccupsoft <dragonmaster.hiccup1991@gmail.com>
2022-04-16 15:14:45 -0400

    second update02

---
+++ build/static/js/22.5b9d1b6c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[22],{1438:function(n,c){},1488:function(n,c){},1490:function(n,c){},1523:function(n,c){},1541:function(n,c){},1543:function(n,c){}}]);
+//# sourceMappingURL=22.5b9d1b6c.chunk.js.map
